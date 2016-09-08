96cb82e5f91f1c708c6c85103dc930df449b357e
mtmcduffie <mcduffie.michael@gmail.com>
2016-09-08 10:34:25 -0400

    UDNTECH-624 - Renaming project and application.

--- idstore-app/urls.py
+++ idstore-app/urls.py
@@ -1,4 +1,4 @@
-"""upload_preprocessing_service URL Configuration
+"""idstore-app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- idstore-app/wsgi.py
+++ idstore-app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for upload_preprocessing_service project.
+WSGI config for idstore-app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "upload_preprocessing_service.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idstore-app.settings")
 
 application = get_wsgi_application()
